0d75210f4a8a2d82f7b425fd886714854549e35b
Jérémy Bolduc <BoldJe1.work@gmail.com>
2021-01-13 15:55:44 -0500

    Fixed problems with css files not being properly inserted

--- webpack.config.js
+++ webpack.config.js
@@ -3,10 +3,21 @@ const buildPath = path.resolve(__dirname, 'nui');
 const sourceDir = path.resolve(__dirname, 'nui', 'src');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
-module.exports = (env, argv)=> {
+module.exports = (env, argv) => {
+    
+    let isProd = argv.mode === 'production';
     return {
+        plugins : [
+            new MiniCssExtractPlugin({
+                // Options similar to the same options in webpackOptions.output
+                // both options are optional
+                filename : path.join('css', 'index.css'),
+                chunkFilename : path.join('css', '[id].css'),
+                linkType : false
+            })
+        ],
         entry : path.join(sourceDir, 'index.ts'),
-        devtool : 'eval',
+        devtool : "inline-source-map",
         output : {
             path : buildPath,
             filename : path.join('scripts', 'index.js')
@@ -20,7 +31,7 @@ module.exports = (env, argv)=> {
                 '.pug'
             ]
         },
-    
+        
         resolveLoader : {
             modules : [
                 path.resolve(__dirname, 'node_modules'),
@@ -28,13 +39,13 @@ module.exports = (env, argv)=> {
                 'node_modules'
             ]
         },
-    
+        
         optimization : {
-            minimize : (argv.mode !== 'production')
+            minimize : isProd
         },
-    
+        
         mode : 'development',
-    
+        
         module : {
             rules : [
                 {
@@ -42,43 +53,41 @@ module.exports = (env, argv)=> {
                     use : ["source-map-loader"],
                     enforce : "pre"
                 },
-            
+                
                 {
                     test : /\.pug$/,
                     use : [
                         {
                             loader : "pug-loader",
                             options : {
-                                pretty : !(argv.mode !== 'production'),
+                                pretty : !isProd,
                             }
                         }
                     ],
                 },
-            
+                
                 {
                     test : /\.s[ac]ss$/i,
                     use : [
-                        argv.mode !== 'production' ? 'style-loader' : MiniCssExtractPlugin.loader,
-                        {
-                            loader : "css-loader",
-                        },
+                        MiniCssExtractPlugin.loader,
+                        "css-loader",
                         {
                             loader : "sass-loader",
                             options : {
                                 implementation : require("sass"),
                                 sassOptions : {
-                                    outputStyle :  argv.mode === 'production' ? "compressed" : "expanded",
+                                    outputStyle : isProd ? "compressed" : "expanded",
                                 }
                             }
                         }
                     ]
                 },
-            
+                
                 {
                     test : /\.(png|jpe?g|gif|otf)$/i,
                     type : 'asset/resource'
                 },
-            
+                
                 {
                     test : /\.tsx?$/,
                     use : [
@@ -88,14 +97,6 @@ module.exports = (env, argv)=> {
                 },
             ]
         },
-    
-        plugins : [
-            new MiniCssExtractPlugin({
-                // Options similar to the same options in webpackOptions.output
-                // both options are optional
-                filename : path.join('css', '[name].css'),
-                chunkFilename : path.join('css', '[id].css'),
-            }),
-        ],
+        
     };
 }
